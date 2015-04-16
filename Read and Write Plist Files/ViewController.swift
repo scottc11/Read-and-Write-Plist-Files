b0c2881a5b5363605f075b4554e960bbb5a376f2
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2015-04-16 11:33:49 -0400

    Initial Commit

---
+++ Read and Write Plist Files/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Read and Write Plist Files
+//
+//  Created by Scott Campbell on 2015-04-16.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
